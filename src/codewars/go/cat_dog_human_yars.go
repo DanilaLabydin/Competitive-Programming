d72a626ed0eea0c71388de82b945f63feedc7c9d
DanilaLabydin <labydin2605@mail.ru>
2025-01-26 17:50:16 +0300

    go and python practices has been added

--- src/codewars/go/cat_dog_human_yars.go
+++ src/codewars/go/cat_dog_human_yars.go
@@ -5,8 +5,9 @@ import (
 )
 
 func main() {
-	test := 2
+	test := 3
 	fmt.Println(CalculateYears(test))
+	fmt.Println(CalculateYears2(test))
 }
 
 func CalculateYears(years int) (result [3]int) {
@@ -24,6 +25,25 @@ func CalculateYears(years int) (result [3]int) {
 		}
 
 	}
+	return result
+}
 
+func CalculateYears2(years int) (result [3]int) {
+	for i:=1; i <= years; i ++ {
+		switch  {
+		case i == 1:
+			result[0] += 1
+			result[1] += 15
+			result[2] += 15
+		case i == 2:
+			result[0] += 1
+			result[1] += 9
+			result[2] += 9
+		default:
+			result[0] += 1
+			result[1] += 4
+			result[2] += 5
+		}
+	}
 	return result
 }
